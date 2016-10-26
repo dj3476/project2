bfa3cc056925a53dda32846984f08842a86cca76
dj3476 <djere001@gmail.com>
2016-10-25 22:17:18 -0700

    created featured products and orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
-
 Rails.application.routes.draw do
   resources :products
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
-	root 'static_pages#index'
+	root 'static_pages#landing_page'
 
   get 'static_pages/index'
+
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
