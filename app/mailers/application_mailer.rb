053fb604d96f3fbf4955001702e143bfe67dac9a
dj3476 <djere001@gmail.com>
2016-11-29 22:38:38 -0800

    5.3 modifications

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'djere001@gmail.com'
+  default from: 'your-email@example.com'
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-	default from: "djere001@gmail.com"
+	default from: "your-email@example.com"
 
-	def contact_form(email, name, message)
-		@message = message
-			mail(:from => email,
-				:to => 'djere001@gmail.com',
-				:subject => "A new contact form message from #{name}")
+  	def contact_form(email, name, message)
+	  	@message = message
+    	mail(:from => email,
+        :to => 'djere001@gmail.com',
+        :subject => "A new contact form message from #{name}")
 	end
 end
