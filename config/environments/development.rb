85eb37549955b0a36d74e71e209a3a200ed4953a
dj3476 <djere001@gmail.com>
2016-11-30 16:45:32 -0800

    5.4 Authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Edit config so dynamic links in emails generate correctly
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
