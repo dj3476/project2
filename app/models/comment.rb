8a5f6cc9b644bfe2eaf9f5c87824ca12eba13f12
dj3476 <djere001@gmail.com>
2016-12-08 19:50:05 -0800

    show page styling

---
+++ app/models/comment.rb
@@ -0,0 +1,9 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  scope :rating_desc, -> { order(rating: :desc) }
+
+  
+  belongs_to :product
+  scope :rating_desc, -> { order(rating: :desc) }
+
+end
