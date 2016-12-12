81bd3dd9ced9eb15e6bb179843c66373820c00ec
dj3476 <djere001@gmail.com>
2016-12-11 22:06:37 -0800

    5.8 raty star rating system

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,14 @@
+$(document).on('turbolinks:load', function(){
+   $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+
+});
+
+$(document).on('turbolinks:load', function(){
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+      readOnly: true,
+      score: function() {
+        return $(this).attr('data-score');
+      }
+    });
+});
