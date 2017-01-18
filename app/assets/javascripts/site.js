b02bef649f9413c679d76a82f7ba341d9221de8b
dj3476 <djere001@gmail.com>
2017-01-18 11:02:38 -0800

    6.10 adding ajax

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,30 @@
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
+    });
+};
+
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
+    $(".alert").delay(4000).fadeOut("slow");
+
+    /* raty plugin */
+    refreshRating();
+
+    /* elevate zoom plugin*/
+    $('.img-zoom').elevateZoom();
+});
+
+/*
 $(document).on('turbolinks:load', function(){
+  console.log($(".alert"));
+  $(".alert").delay(4000).fadeOut("slow");
+});
+/*$(document).on('turbolinks:load', function(){
 	console.log($(".alert"));
   	$(".alert").delay(2500).fadeOut("slow");
 
@@ -14,6 +40,6 @@ $(document).on('turbolinks:load', function(){
       }
     });
 
-    /*elevate zoom plugin*/
+    elevate zoom plugin
     $(".img-zoom").elevateZoom();
-});
+});*/
